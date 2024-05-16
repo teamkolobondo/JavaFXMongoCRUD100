b9086e463a730c92238e1070435588fec1efa903
teamkolobondo <daniel.jimenez@campusfp.es>
2024-05-16 14:40:48 +0200

    mostrar datos

--- src/main/java/com/empresa/javafxmongocrud100/HelloController.java
+++ src/main/java/com/empresa/javafxmongocrud100/HelloController.java
@@ -1,8 +1,13 @@
 package com.empresa.javafxmongocrud100;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import org.bson.Document;
 
 public class HelloController {
     @FXML
@@ -16,6 +21,16 @@ public class HelloController {
     }
     @FXML
     protected void mostrar(){
+        String url="mongodb+srv://administrador:Abc123456@teamkolobondo.zj7al6y.mongodb.net/";
+        MongoClient mongoClient = MongoClients.create(url);
+        MongoDatabase database = mongoClient.getDatabase("actividad17");
+        MongoCollection<Document> collection = database.getCollection("clientes");
+
+        for (Document doc : collection.find()) {
+
+            lv_datos.getItems().add(doc);
+        }
+
 
     }
 }//Ciera la clase
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.empresa.javafxmongocrud100 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires mongo.java.driver;
 
 
     opens com.empresa.javafxmongocrud100 to javafx.fxml;
