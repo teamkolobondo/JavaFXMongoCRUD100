496b56ac267a15196081f90eb9e4fdbe658fe5c0
teamkolobondo <daniel.jimenez@campusfp.es>
2024-05-16 13:50:13 +0200

    capa visual mostrar en ListView

--- src/main/java/com/empresa/javafxmongocrud100/HelloController.java
+++ src/main/java/com/empresa/javafxmongocrud100/HelloController.java
@@ -2,13 +2,20 @@ package com.empresa.javafxmongocrud100;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private ListView lv_datos;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
-}
+    @FXML
+    protected void mostrar(){
+
+    }
+}//Ciera la clase
